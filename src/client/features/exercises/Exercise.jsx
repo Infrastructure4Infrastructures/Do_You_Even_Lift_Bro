59b453ed8112037f8cbc6185fb84651c8d762325
Charles Bryant <cwbryant25@yahoo.com>
2024-03-28 14:16:43 -0700

    Workout page updated

--- src/client/features/exercises/Exercise.jsx
+++ src/client/features/exercises/Exercise.jsx
@@ -26,24 +26,37 @@ export default function Exercise({ exercise }) {
   };
 
   return (
-    <li>
-      <form onSubmit={save}>
-        <input
-          type='checkbox'
-          checked={exercise.done}
-          onChange={toggleExercise}
-        />
-        <input
-          type='text'
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          required
-        />
-        <button>Save</button>
-        <button onClick={onDelete} aria-label='delete'>
-          🞪
-        </button>
-      </form>
-    </li>
+    <div>
+      <h2>Description of Exercise here</h2>
+      <table>
+        <tr>
+          <th>Exercise</th>
+          <th>Sets Goal</th>
+          <th>Reps Goal</th>
+          <th>My Sets</th>
+          <th>My Reps</th>
+          <th>Delete Entry</th>
+        </tr>
+        <tr>
+          <td>Exercise Name Goes Here</td>
+          <td>Set Number Goes Here</td>
+          <td>Reps Number Goes Here</td>
+          <td>
+            {/* My Sets input box */}
+            <label for='My Sets'>
+              <input type='number' id='sets' />
+            </label>
+          </td>
+          <td>
+            {/* My Reps input box */}
+            <label for='My Reps'>
+              <input type='number' id='reps' />
+            </label>
+          </td>
+          <td>Delete Box</td>
+        </tr>
+      </table>
+      <button id='addBtn'>Add Additional Set</button>
+    </div>
   );
 }
