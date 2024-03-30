6ecbbd98678be68bf3e0860d67a923d05ec5adcc
Charles Bryant <cwbryant25@yahoo.com>
2024-03-30 15:47:24 -0700

    Meal page update

--- src/client/features/meals/Meal.jsx
+++ src/client/features/meals/Meal.jsx
@@ -1,47 +1,65 @@
-import { useState } from "react";
-import { useDeleteMealMutation, useEditMealMutation } from "./mealSlice";
+// import { useState } from "react";
+// import { useDeleteMealMutation, useEditMealMutation } from "./mealSlice";
 // import { meal } from "../../../server/prisma";
 
+// import { useSelector } from "react-redux";
+import { Fragment } from "react";
+import { useDeleteMealMutation, useGetMealsQuery } from "./mealSlice";
+// import { selectToken } from "../auth/authSlice";
+
 /** Allows user to read, update, and delete a meal */
-export default function Meal({ meal }) {
-  const [editMeal] = useEditMealMutation();
+export default function Meal({}) {
+  const { data: meals } = useGetMealsQuery();
   const [deleteMeal] = useDeleteMealMutation();
+  console.log(meals);
+  // const token = useSelector(selectToken);
 
-  const [description, setDescription] = useState(meal.description);
+  // const [editMeal] = useEditMealMutation();
+  // const [deleteMeal] = useDeleteMealMutation();
 
-  /** Updates the meal's `done` status */
-  const toggleMeal = async (evt) => {
-    const done = evt.target.checked;
-    editMeal({ ...meal, done });
-  };
+  // const [description, setDescription] = useState(meal.description);
 
-  /** Saves the food meal */
-  const save = async (evt) => {
-    evt.preventDefault();
-    editMeal({ ...meal, description });
-  };
+  // /** Updates the meal's `done` status */
+  // const toggleMeal = async (evt) => {
+  //   const done = evt.target.checked;
+  //   editMeal({ ...meal, done });
+  // };
+
+  // /** Saves the food meal */
+  // const save = async (evt) => {
+  //   evt.preventDefault();
+  //   editMeal({ ...meal, description });
+  // };
 
   /** Deletes the meal */
-  const onDelete = async (evt) => {
+  const onDelete = async (mealId, evt) => {
     evt.preventDefault();
-    deleteMeal(meal.id);
+    deleteMeal(mealId);
   };
 
   return (
-    <li>
-      <form onSubmit={save}>
-        <input type='checkbox' checked={meal.done} onChange={toggleMeal} />
-        <input
-          type='text'
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          required
-        />
-        <button>Save</button>
-        <button onClick={onDelete} aria-label='delete'>
-          🞪
-        </button>
-      </form>
-    </li>
+    <div className='meals_container'>
+      <h2>Food Journal</h2>
+      <tr>
+        <th>Date/Time</th>
+        <th>Food Entry</th>
+        <th>Calories</th>
+        <th>Remove Entry</th>
+      </tr>
+      {meals.map((meal) => (
+        <tr key={meal.id}>
+          {meal.Meal_Food_Items.map((Meal_Food_Items, index) => (
+            <Fragment key={index}>
+              <td>{Meal_Food_Items.food_item.date}</td>
+              <td>{Meal_Food_Items.food_item.foodEntry}</td>
+              <td>{Meal_Food_Items.food_item.calories}</td>
+            </Fragment>
+          ))}
+          <td>
+            <button onClick={onDelete}>X</button>
+          </td>
+        </tr>
+      ))}
+    </div>
   );
 }
--- src/client/features/meals/Meals.jsx
+++ src/client/features/meals/Meals.jsx
@@ -1,60 +1,68 @@
 import { useSelector } from "react-redux";
 import { selectToken } from "../auth/authSlice";
-// import NewTask from "./NewTask";
-// import Meal from "./Meal";
+import { useState } from "react";
+import Meal from "./Meal.jsx";
 import { useCreateMealMutation } from "./mealSlice";
 
 export default function Meals() {
   const token = useSelector(selectToken);
-  // const [string, setString] = useState("");
+  const [foodEntry, setFoodEntry] = useState("");
+  const [calories, setCalories] = useState("");
+  const [date, setDate] = useState("");
+
   const [createMeal] = useCreateMealMutation();
 
   // const dispatch = useDispatch();
 
   const addFood = (event) => {
     event.preventDefault();
-    createMeal({ meal });
+    createMeal({ foodEntry, calories, date });
+    setFoodEntry("");
+    setCalories("");
+    setDate("");
   };
-  // if (!token) {
-  //   return <p>You must be logged in to see meals.</p>;
-  // }
-
+  if (!token) {
+    return <p>You must be logged in to see meals.</p>;
+  }
   return (
-    <div className='meals'>
+    <div>
       <h3>
-        {" "}
         Track your meals in the input below. Click "Add Meal" to add a new meal
-        entry.{" "}
+        entry.
       </h3>
       <form onSubmit={addFood}>
-        <label for='foodEntry'>Food Entry</label>
-        <input type='text' name='foodEntry' />
-        <label for='Calories'>Calories: </label>
-        <input type='text' name='calories' />
-        <label for='dateTime'> Date/Time: </label>
-        <input type='dateTime' name='dateTime' />
-        <button onClick='/food_item/' method='POST'>
+        <label for='foodEntry'>
+          Food Entry:
+          <input
+            type='text'
+            value={foodEntry}
+            onChange={(e) => setFoodEntry(e.target.value)}
+          />
+        </label>
+
+        <label for='Calories'>
+          Calories:
+          <input
+            type='text'
+            value={calories}
+            onChange={(e) => setCalories(e.target.value)}
+          />
+        </label>
+
+        <label for='dateTime'>
+          Date/Time:
+          <input
+            type='dateTime'
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </label>
+
+        <button onClick='/food_item' method='POST'>
           Add Food
         </button>
       </form>
-
-      <h2>Food Journal</h2>
-      <tr>
-        <th>Date/Time</th>
-        <th>Food Entry</th>
-        <th>Calories</th>
-        <th>Remove Entry</th>
-      </tr>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td>
-          <button onClick='/food_item' method='DELETE' name='deleteFood'>
-            X
-          </button>
-        </td>
-      </tr>
+      <Meal />
     </div>
   );
 }
