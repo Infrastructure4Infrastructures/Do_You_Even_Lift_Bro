b1a916f0fe39536878050c92891c543d255cbe41
Charles Bryant <cwbryant25@yahoo.com>
2024-04-03 10:55:00 -0700

    repsGoal array added, delete work in progress

--- src/client/features/exercises/Exercises.jsx
+++ src/client/features/exercises/Exercises.jsx
@@ -36,12 +36,6 @@ export default function Exercises({ difficulty }) {
     return <div>Loading...</div>;
   }
 
-  // Create a async function that takes a parameter (exerciseId)
-  const onDelete = async (exerciseId) => {
-    // Execute the deleteExercise function using the useDeleteExerciseMutation hook on exerciseId
-    deleteExercise(exerciseId);
-  };
-
   // Use an arrow function handle clicking the next button for the next exercise
   const handleNext = () => {
     // Using a callback function taking in a parameter of the current index
@@ -87,38 +81,59 @@ export default function Exercises({ difficulty }) {
     // Push every iteration of setsGoal to the empty array of rows
     rows.push(setsGoal);
   }
+
+  const repsGoal = [];
+  // Loop through the rows array
+  for (const ele of rows) {
+    let updatedRows =
+      /* At each iteration, if ele equals exerciseRepsGoal(which is 15) if true, assign that ele the same index, if false, multiply 1 by exerciseRepsGoal(15) */
+      ele === exerciseRepsGoal[0] ? exerciseRepsGoal : 1 * exerciseRepsGoal;
+    repsGoal.push(updatedRows);
+  }
+
+  console.log(rows);
+  console.log(repsGoal);
+
+  // Create a async function that takes a parameter (exerciseId)
+  const onDelete = async (exerciseId) => {
+    // Execute the deleteExercise function using the useDeleteExerciseMutation hook on exerciseId
+    deleteExercise(exerciseId);
+  };
+
   return (
     <div>
       <main>
-
-        <section class="buttonsect">
-        <a id="">
-          <button id="prebut" onClick={handlePrevious}>Previous</button>
-        </a>
-        <a>
-          <button id="nextbut" onClick={handleNext}>Next</button>
-        </a>
+        <section class='buttonsect'>
+          <a id=''>
+            <button id='prebut' onClick={handlePrevious}>
+              Previous
+            </button>
+          </a>
+          <a>
+            <button id='nextbut' onClick={handleNext}>
+              Next
+            </button>
+          </a>
         </section>
 
-        <section class="exnamesec">
-        <h2 class="exname">
-          {exercise.name.charAt(0).toUpperCase() + exercise.name.slice(1)}
-        </h2>
+        <section class='exnamesec'>
+          <h2 class='exname'>
+            {exercise.name.charAt(0).toUpperCase() + exercise.name.slice(1)}
+          </h2>
         </section>
 
-        <section class="video">
-        <iframe 
-          width='560'
-          height='315'
-          src={exerciseVideo}
-          title='{exercise.name} Video'
-          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-          allowFullScreen
-        ></iframe>
+        <section class='video'>
+          <iframe
+            width='560'
+            height='315'
+            src={exerciseVideo}
+            title='{exercise.name} Video'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+            allowFullScreen
+          ></iframe>
         </section>
-
       </main>
-      <h3 class="exdesc">{exerciseDescription}</h3>
+      <h3 class='exdesc'>{exerciseDescription}</h3>
       <table>
         <thead>
           <tr>
@@ -129,11 +144,11 @@ export default function Exercises({ difficulty }) {
             <th>Delete Entry</th>
           </tr>
         </thead>
-        <tbody class="tbodyex">
+        <tbody class='tbodyex'>
           {/* Map over the rows array with two paramters, row and index */}
           {rows.map((row, index) => (
             // Pass in the current index as a prop
-            <tr class="toprowtablerow" key={index}>
+            <tr class='toprowtablerow' key={index}>
               {/* Render to the page the following table tags based of the setsGoal number */}
               <td>{exerciseName}</td>
               <td>{row}</td>
@@ -153,12 +168,16 @@ export default function Exercises({ difficulty }) {
                 >
                   X
                 </button> */}
-                <button  id='addBtn'
+                <button
+                  id='addBtn'
                   onClick={onDelete}
                   method='DELETE'
                   name='deleteExercise'
-                  class="button-24" 
-                  role="button">Delete</button>
+                  class='button-24'
+                  role='button'
+                >
+                  Delete
+                </button>
               </td>
             </tr>
           ))}
@@ -167,14 +186,18 @@ export default function Exercises({ difficulty }) {
       <table>
         <tbody>
           <tr>
-            <td class="bottomTabBut">
+            <td class='bottomTabBut'>
               {/* Need to add onClick Function to add a set */}
-              <button  class="button-91" role="button">Add Another Set</button>
+              <button class='button-91' role='button'>
+                Add Another Set
+              </button>
             </td>
             {/* onClick={addSet} method='POST' */}
             <td>
               {/* Need to add onClick Function to submit workout */}
-              <button class="button-91" role="button">Submit Workout</button>
+              <button class='button-91' role='button'>
+                Submit Workout
+              </button>
             </td>
             {/* onClick={submitworkout} method='POST' */}
           </tr>
