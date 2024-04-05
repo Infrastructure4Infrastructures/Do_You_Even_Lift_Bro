1e575b1428041dad5ca76c59f7c0c96f3a26d361
Charles Bryant <cwbryant25@yahoo.com>
2024-04-05 01:02:36 -0700

    Exercise page updated for exercsie journal

--- src/client/features/exercises/ExerciseJournal.jsx
+++ src/client/features/exercises/ExerciseJournal.jsx
@@ -1,40 +1,25 @@
-// import { useNavigate, useParams } from "react-router-dom";
-// import { useSelector } from "react-redux";
-// import { selectToken } from "../auth/authSlice";
-// import "./journal.css";
+import { useNavigate, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectToken } from "../auth/authSlice";
 
-// import { useGetJournalQuery } from "./journalSlice";
-// import { useDeleteJournalEntryMutation } from "../journal_entry/journalEntrySlice";
+import {
+  useDeleteUsersExerciseMutation,
+  useGetUsersExerciseQuery,
+} from "../user_exercises/user_exercisesSlice";
+import { useGetExercisesByIdQuery } from "../exercises/exercisesSlice";
 
 export default function ExerciseJournal() {
-  //   const { data: journal } = useGetJournalQuery();
-  //   const [deleteJournal_Entry] = useDeleteJournalEntryMutation();
+  const { data: user_exercises, isLoading } = useGetUsersExerciseQuery();
+  const [deleteEntry] = useDeleteUsersExerciseMutation();
 
-  //   const token = useSelector(selectToken);
-  //   const navigate = useNavigate([]);
+  const token = useSelector(selectToken);
+  // const navigate = useNavigate([]);
 
-  //   const { id } = useParams();
+  const { id } = useParams();
 
-  //   // const journal = journal_entry?.find((journal) => journal._id === id);
-  //   console.log(journal);
-  //   if (!token) {
-  //     return "Must be logged in to view JournaL Entries!";
-  //   }
-
-  //   // const [description, setDescription] = useState(meal.description);
-
-  //   // /** Saves the food meal */
-  //   // const save = async (evt) => {
-  //   //   evt.preventDefault();
-  //   //   editMeal({ ...meal, description });
-  //   // };
-
-  //   /** Deletes the meal */
-  //   const onDelete = async (id) => {
-  //     // evt.preventDefault();
-  //     await deleteJournal_Entry(id);
-  //     navigate("/meals");
-  //   };
+  if (!token) {
+    return "Must be logged in to view JournaL Entries!";
+  }
 
   // Create a async function that takes a parameter (exerciseId)
   const onDelete = async (id) => {
@@ -43,55 +28,45 @@ export default function ExerciseJournal() {
     //   let newSetsGoal = [];
     //   if (setsGoal >= exerciseSetsGoal) {
     // e.preventDefault();
-    editExercise(id);
-    console.log(id);
+    await deleteEntry(id);
   };
 
   return (
     <div className='journal_entry_container'>
-      <h2 class='fjTitle'>Exercise Journal</h2>
-      <table class='foodJournalFormName'>
-        <thead>
-          <tr>
-            <th class='ftInpLab'>Exercise</th>
-            <th class='ftInpLab'>Sets</th>
-            <th class='ftInpLab'>Reps</th>
-
-            <th class='ftInpLab'>Remove Entry</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            {/* {journal_entry &&
-            (() => {
-              const journalEntries = [];
-              for (let i = 0; i < journal_entry.length; i++) {
-                const entry = journal_entry[i];
-                journalEntries.push( */}
-            {/* {journal?.[0]?.Journal_Entry.map((entry, i) => (
-           
-              <td>{entry.date}</td>
-              <td>{entry.note}</td>
-              {/* <td>{entry.food_item.description}</td>
-                    <td>{entry.food_item.calories}</td> */}
-            <td>
-              <button
-                onClick={() => onDelete(entry.id)}
-                className='button-24'
-                role='button'
-                aria-label='DELETE'
-              >
-                Delete
-              </button>
-            </td>
-          </tr>
-          {/* ))} */}
-          {/* );
-              }
-              return journalEntries;
-            })()} */}
-        </tbody>
-      </table>
+      <h2 className='fjTitle'>Exercise Journal</h2>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <table className='foodJournalFormName'>
+          <thead>
+            <tr>
+              <th className='ftInpLab'>Exercise</th>
+              <th className='ftInpLab'>Sets</th>
+              <th className='ftInpLab'>Reps</th>
+              <th className='ftInpLab'>Remove Entry</th>
+            </tr>
+          </thead>
+          <tbody>
+            {user_exercises.map((entry, index) => (
+              <tr key={index}>
+                <td>{entry.name}</td>
+                <td>{entry.mySets}</td>
+                <td>{entry.myReps}</td>
+                <td>
+                  <button
+                    onClick={() => onDelete(entry.id)}
+                    className='button-24'
+                    role='button'
+                    aria-label='DELETE'
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
