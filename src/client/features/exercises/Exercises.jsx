60a08e869eb297c1d33f9d3f946ddca9eea43c7e
Charles Bryant <cwbryant25@yahoo.com>
2024-04-05 06:23:44 -0700

    Cleaned up code

--- src/client/features/exercises/Exercises.jsx
+++ src/client/features/exercises/Exercises.jsx
@@ -1,6 +1,5 @@
 import React, { Fragment, useState } from "react";
-// import { useDeleteExercisesByIdMutation } from "./exercisesSlice";
-import { useEditUsersExerciseMutation } from "../user_exercises/user_exercisesSlice";
+
 import {
   useGetWorkoutsBeginnerQuery,
   useGetWorkoutsIntermediateQuery,
@@ -12,14 +11,12 @@ import "./exercises.css";
 
 export default function Exercises({ difficulty }) {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [editExercise] = useEditUsersExerciseMutation();
+
   const [createExercise] = useCreateUsersExerciseMutation();
   const [myReps, setMyReps] = useState([]);
 
   let selectedDifficulty = null;
 
-  // Check if the selected difficulty is strictly equal to one of the 3 given difficulties
-  // If so, use the corresponding getWorkouts hook to render the exercises to the page
   if (difficulty === "beginner") {
     selectedDifficulty = useGetWorkoutsBeginnerQuery();
   } else if (difficulty === "intermediate") {
@@ -30,7 +27,6 @@ export default function Exercises({ difficulty }) {
     return <div>No workouts available</div>;
   }
 
-  // Render the data from the given getWorkouts hook and assign it the variable selectedDifficulty
   const { data: workouts, isLoading } = selectedDifficulty;
 
   if (!workouts) {
@@ -40,74 +36,44 @@ export default function Exercises({ difficulty }) {
     return <div>Loading...</div>;
   }
 
-  // Use an arrow function handle clicking the next button for the next exercise
   const handleNext = () => {
-    // Using a callback function taking in a parameter of the current index
     setCurrentIndex((current) => {
-      // Check if the current index is one less the workouts array
       if (current < workouts.length - 1) {
-        // If so, go to the next index
         return current + 1;
       } else {
-        // if not, stay at the current index and do nothing
-        return current;
-      }
-    });
-  };
-  // Use an arrow function handle clicking the previous button for the previous exercise
-  const handlePrevious = () => {
-    // Using a callback function taking in a parameter of the current index
-    setCurrentIndex((current) => {
-      // Check if the current index is greater than 0
-      if (current > 0) {
-        // If so, go to the previous index
-        return current - 1;
-      } else {
-        // if not, stay at the current index and do nothing
         return current;
       }
     });
   };
 
-  // Declare the variable exercise and assign it to the currentIndex of the workouts array
+  const handlePrevious = () => {
+    setCurrentIndex((current) => {
+      if (current > 0) {
+        return current - 1;
+      } else {
+        return current;
+      }
+    });
+  };
+
   const exercise = workouts[currentIndex];
-  // These variable defines using dot notation and bracket notation
   let exerciseDescription = exercise.Workout_Exercises[0].exercises.description;
   let exerciseVideo = exercise.Workout_Exercises[0].exercises.video;
   let exerciseName = exercise.name;
   let exerciseSetsGoal = exercise.Workout_Exercises[0].setsGoals;
   let exerciseRepsGoal = exercise.Workout_Exercises[0].repsGoals;
 
-  // Declare rows to an empty array
   const rows = [];
-  // Using a for loop iterate starting at 1 to the given setsGoal number
   for (let setsGoal = 1; setsGoal <= exerciseSetsGoal; setsGoal++) {
-    // Push every iteration of setsGoal to the empty array of rows
     rows.push(setsGoal);
   }
 
   const repsGoal = [];
-  // Loop through the rows array
   for (const ele of rows) {
     let updatedRows =
-      /* At each iteration, if ele equals exerciseRepsGoal(which is 15) if true, assign that ele the same index, if false, multiply 1 by exerciseRepsGoal(15) */
       ele === exerciseRepsGoal[0] ? exerciseRepsGoal : 1 * exerciseRepsGoal;
     repsGoal.push(updatedRows);
   }
-
-  // // Create a async function that takes a parameter (exerciseId)
-  // const onDelete = async (id) => {
-  //   // Execute the deleteExercise function using the useDeleteExerciseMutation hook on exerciseexerciseexercise
-  //   // for (const ele of rows) {
-  //   //   let newSetsGoal = [];
-  //   //   if (setsGoal >= exerciseSetsGoal) {
-  //   // e.preventDefault();
-  //   editExercise(id);
-  //   console.log(id);
-  // };
-
-  console.log(rows);
-  console.log(repsGoal);
 
   const submitWorkout = async (e) => {
     e.preventDefault();
@@ -133,10 +99,8 @@ export default function Exercises({ difficulty }) {
       myReps: allReps.reduce((acc, cur) => acc + cur, 0),
     });
     setMyReps([]);
-    console.log(allReps);
   };
-  console.log(exercise.id);
-  console.log(exercise);
+
   return (
     <div>
       <main>
@@ -183,7 +147,7 @@ export default function Exercises({ difficulty }) {
             </tr>
           </thead>
           <tbody class='tbodyex'>
-            {/* Map over the rows array with two paramters, row and index */}
+            {/* Map over the rows array with two parameters, row and index */}
             {rows.map((row, index) => (
               // Pass in the current index as a prop
               <tr class='toprowtablerow' key={index}>
@@ -192,25 +156,17 @@ export default function Exercises({ difficulty }) {
                 <td>{row}</td>
                 <td>{exerciseRepsGoal}</td>
                 <td>
-                  {/* My Reps input box */}
                   <label name='reps'>
                     <input
                       type='number'
                       value={myReps}
                       onChange={(e) => setMyReps(e.target.value)}
                       id='reps'
+                      placeholder='Enter data in window prompt'
                     />
                   </label>
                 </td>
                 <td>
-                  {/* <button
-                  id='addBtn'
-                  onClick={onDelete}
-                  method='DELETE'
-                  name='deleteExercise'
-                >
-                  X
-                </button> */}
                   {/* <button
                     id='addBtn'
                     onClick={() => onDelete(exercise.id)}
